d5dd350177af2850cefabb5a8aa4b358e4e3a302
thom page <thom.page.000@gmail.com>
2015-08-11 17:25:13 -0400

    5:30 Tuesday commit - working

--- server.js
+++ server.js
@@ -43,15 +43,20 @@ app.get('/', function (req, res){
 // Session
 app.get('/session', function (req, res) {
 	if (req.session.name) {
-		res.send(req.session)
+		res.send(req.session);
 	} else {
-		res.send('no session')
+		res.send('no session');
 	}
 })
 
 app.post('/setuser', function (req, res) {
 	req.session.name = req.body.name;
 	res.send('Session set for ' + req.session.name);
+})
+
+app.delete('/removeuser', function (req, res) {
+	req.session.name = null
+	res.redirect('/session')
 })
 
 
