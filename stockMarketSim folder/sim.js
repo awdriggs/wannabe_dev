9a99361826114e1f71422b15ff9225bc67344bd7
zhenting zhou <zhentzhou@gmail.com>
2015-08-11 23:59:45 -0400

    more sim update

---
+++ stockMarketSim folder/sim.js
@@ -0,0 +1,41 @@
+//{tradeLedger} JSON Obj, stores everytrade
+var tradeLedger = {
+	"trades":[
+    	{"buyer":"Bot1", "seller":"Bot2", "timeOfTrade":1439254635102, "price":50, "stock":'GOOGL'}
+	]
+};
+//{stockListing} JSON obj, stores the current value of stock
+var stockListing = {
+	"market":[
+		{"GOOGL": 50}
+	]
+};
+//{twitterTrend} basic obj, updates via twitter API
+var twitterTrend = {
+	"GOOGL": 0
+};
+
+//set test variable enviroment
+var tweetCounter = 0;
+var bot1 = new Trader('R2D2', 3000, 'pump'); 
+var bot2 = new Trader('C3PO' ,1000, 'dump');
+var marketMakerBot = new MarketMaker([bot1, bot2]); 
+marketMakerBot.service();
+
+var refreshIntervalId = setInterval( function() { 
+	console.log("---------- ---------- ---------- ---------- ----------"); 
+	console.log("Twitter API returning data..."); 
+	function getRandomArbitrary(min, max) {
+	    return Math.random() * (max - min) + min;
+	}
+	twitterTrend.GOOGL = getRandomArbitrary(-10, 10);
+	console.log('>> >> >> >> $GOOGL current price: ' + stockListing.market[0].GOOGL + ' << << << <<');
+	tweetCounter = tweetCounter + 1;
+
+    if (tweetCounter == 10) {
+    	clearInterval(refreshIntervalId);
+    	console.log("sim ended.")
+    };
+}, 2000);
+
+
