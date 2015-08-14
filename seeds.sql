cc75c713f433b789332324e04c5148095dae1629
thom page <thom.page.000@gmail.com>
2015-08-13 20:27:20 -0400

    dummy seeds to configure

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,13 @@
-INSERT INTO users (username, password, companyname, balance, bot) VALUES ('Thom', 'brooklyndonuts', 'Wannabe Corp', 1000, 'Real MVP');
-INSERT INTO users (username, password, companyname, balance, bot) VALUES ('Adam', 'brooklyndonuts', 'Wannabe Corp', 1000, 'MathBot');
-INSERT INTO users (username, password, companyname, balance, bot) VALUES ('Zhen', 'queensdonuts', 'Wannabe Corp', 1000, 'Da King');
+INSERT INTO bots (botname, balance, character, stockinterest, quantity, risktolerance, stepsize, attitude, active, "userId", "stockId", "companyId") VALUES ('Jimmy', 1000, 'pumper', '$GOOGL', 500, 5, 5, 5, 'True', 0, 1, 1);
+INSERT INTO bots (botname, balance, character, stockinterest, quantity, risktolerance, stepsize, attitude, active, "userId", "stockId", "companyId") VALUES ('Roger', 1000, 'pumper', '$GOOGL', 500, 5, 5, 5, 'True', 0, 1, 2);
+INSERT INTO bots (botname, balance, character, stockinterest, quantity, risktolerance, stepsize, attitude, active, "userId", "stockId", "companyId") VALUES ('Tim', 1000, 'pumper', '$AAPL', 500, 5, 5, 5, 'True', 0, 2, 1);
+
+INSERT INTO companies (name, startingbalance, currentbalance, profit) VALUES ('BlahCorp', 1000, 1000, 0);
+INSERT INTO companies (name, startingbalance, currentbalance, profit) VALUES ('SmellCorp', 1000, 1000, 0);
+
+INSERT INTO stocks (name, price) VALUES('$GOOGL', 55);
+INSERT INTO stocks (name, price) VALUES('$AAPL', 55);
+
+INSERT INTO users (username, password, companyname, currentbalance, startingbalance, profit) VALUES ('Thom', 'brooklyndonuts', 'TinyMountain', 1000, 1000, 0);
+INSERT INTO users (username, password, companyname, currentbalance, startingbalance, profit) VALUES ('Adam', 'brooklyndonuts', 'NascentBreathers', 1000, 1000, 0);
+INSERT INTO users (username, password, companyname, currentbalance, startingbalance, profit) VALUES ('Zhen', 'queensdonuts', 'SlidingVines', 1000, 1000, 0);
