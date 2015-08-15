d750be12ea9b090daea51060827f73ae22bdc7f5
thom page <thom.page.000@gmail.com>
2015-08-15 16:54:45 -0400

    done debugging the sim for now

--- stockMarketSim_folder/tradeLogic.js
+++ stockMarketSim_folder/tradeLogic.js
@@ -1,5 +1,5 @@
-var tradeLogic = function (botChar, self, marketPrice, oldTrendVal, newTrendVal) {
-    
+var tradeLogic = function (botChar, self, marketPriceInput, oldTrendVal, newTrendVal) {
+    this.marketPrice = parseFloat(marketPriceInput);
     console.log('tradeLogic loaded...');
 	//setup behavior based on characteristics 
 	switch (botChar) {
@@ -20,7 +20,7 @@ var tradeLogic = function (botChar, self, marketPrice, oldTrendVal, newTrendVal)
             self.orderType = 'SELL';
             self.lookingForTrade = true;
             self.urgency = -1;
-            console.log(self.name + ' is a constant buyer, looking for trade: for $' + ',' +  self.offerPrice + self.lookingForTrade + ', urgency: [' + self.urgency + ']');
+            console.log(self.name + ' is a constant buyer, looking for trade: for $' +  self.offerPrice + ',' + self.lookingForTrade + ', urgency: [' + self.urgency + ']');
         break;
 
         // basic marketTrader bot will buy or sell using basic logic
@@ -33,22 +33,24 @@ var tradeLogic = function (botChar, self, marketPrice, oldTrendVal, newTrendVal)
             };
             var absVal = Math.abs(newTrendVal);
             //interested to do something
-            if (absVal >= 0 && absVal <= 3) {
+            if (absVal >= 0 && absVal < 3) {
                 if (self.orderType == 'BUY') {
                     self.offerPrice = marketPrice + 0.25;
                 }else{
                     self.offerPrice = marketPrice - 0.25;
                 };
                 self.urgency = 1;
+                console.log("current market price is from market trader:" + marketPrice);
                 console.log(self.name + " kind of want to " + self.orderType + ' $' + self.interests + " for $" + self.offerPrice + ', urgency: [' + self.urgency + ']');
             //desired to do something
-            }else if (absVal > 3 && absVal < 6) {
+            }else if (absVal >= 3 && absVal < 6) {
                 if (self.orderType == 'BUY') {
                     self.offerPrice = marketPrice + 1.50;
                 }else{
                     self.offerPrice = marketPrice - 1.50;
                 };
                 self.urgency = 5;
+                console.log("current market price is from market trader:" + marketPrice);
                 console.log(self.name + " really want to " + self.orderType + ' $' + self.interests + " for $" + self.offerPrice + ', urgency: [' + self.urgency + ']');
             //desperate to do something
             }else if (absVal > 6) {
@@ -58,6 +60,8 @@ var tradeLogic = function (botChar, self, marketPrice, oldTrendVal, newTrendVal)
                     self.offerPrice = marketPrice - 3;
                 };
                 self.urgency = 10;
+
+                console.log("current market price is from market trader:" + marketPrice);
                 console.log(self.name + " desperatelly want to " + self.orderType + ' $' + self.interests + " for $" + self.offerPrice + ', urgency: [' + self.urgency + ']');
             };
             self.lookingForTrade = true;
