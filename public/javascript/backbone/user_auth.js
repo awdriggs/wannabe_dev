2718d8529b08bb4c4f1e022bb8a8572584553230
Adam Driggers <awdriggs@gmail.com>
2015-08-17 11:31:32 -0400

    fixed the bot update form so it works in the template

--- public/javascript/backbone/user_auth.js
+++ public/javascript/backbone/user_auth.js
@@ -28,6 +28,78 @@ var getSession = function() {
                 $('#input_ui').empty();
                 App.currentUserView = new App.Views.CurrentUserView();
                 //show the user info template
+
+
+                $(".risk-slider").slider({
+                    min: 0,
+                    max: 10,
+                    slide: function(event, ui) {
+                        $("#risk").text(ui.value);
+                    }
+                });
+
+
+                $(".trend-slider").slider({
+                        min: 0,
+                        max: 10,
+                        slide: function(event, ui) {
+                            $("#trend").text(ui.value);
+                        }
+                    });
+
+                    $(".attitude-slider").slider({
+                        min: 0,
+                        max: 10,
+                        slide: function(event, ui) {
+                            $("#attitude").text(ui.value);
+                        }
+                    });
+
+                $('#setbot').click(function() {
+                    var bot_params = {
+                        botname: $("input[name='botname']").val(),
+                        balance: parseInt($("input[name='balance']").val()),
+                        character: $("select[name='character']").val(),
+                        interest: $("select[name='interest']").val(),
+                        risk: parseInt($("#risk").text()),
+                        stepsize: parseInt($("#trend").text()),
+                        attitude: parseInt($("#attitude").text())
+                    }
+                    console.log(bot_params)
+
+                    // Instantiate new bot model
+                    App.bot = new App.Models.Bot;
+                    // Set all default attributes for simulator
+                    App.bot.set({
+                        quantity: 0,
+                        active: 'True',
+                        userId: 1,
+                        stockId: 1,
+                        company: 0
+                    });
+                    // Set all user-defined attributes
+                    App.bot.set({
+                        botname: bot_params.botname,
+                        balance: bot_params.balance,
+                        character: bot_params.character,
+                        stockinterest: bot_params.interest,
+                        risktolerance: bot_params.risk,
+                        stepsize: bot_params.stepsize,
+                        attitude: bot_params.attitude
+                    });
+
+                    App.bot.save();
+                    console.log('bot SHOULD have saved');
+
+                });
+
+                //emitter for the set/update of the bot
+                $('#setbot').click(function() {
+                    console.log('spitter');
+                    socket.emit('change_bot', "working?"); //need to write script to handle is server
+                });
+
+
                 //add a button to the button of the sidebar for logout, kills the session
                 //no user
                 return true;
