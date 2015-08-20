5c93b6b6969cd20167fd7345ffd105a8800adddd
Adam Driggers <awdriggs@gmail.com>
2015-08-20 12:33:39 -0400

    took out the user auth

--- public/javascript/backbone/views/currentuserview.js
+++ public/javascript/backbone/views/currentuserview.js
@@ -1,6 +1,6 @@
 App.Views.CurrentUserView = Backbone.View.extend({
-	el: '#edit_area',
-
+	//el: '#edit_area',
+	el: '#input_ui', //change this back to the above code when having user sesions.
 	initialize: function () {
 		console.log('single login view created');
 		this.template = Handlebars.compile($('#newbottemplate').html());
