e63f6c833df38ccda86b2fd8ea5abd622d32e33b
thom page <thom.page.000@gmail.com>
2015-09-12 12:05:59 -0400

    clicks removed

--- public/javascript/backbone/views/botsview.js
+++ public/javascript/backbone/views/botsview.js
@@ -43,29 +43,29 @@ App.Views.BotsView = Backbone.View.extend({
 			// show the bot that was clicked after having cleared the page
 			var botview = new App.Views.BotAssocView({ model: botmodel });
 
-			if (botmodel.attributes.companyId != 0) {
-				var companyname = botmodel.attributes.company.name,
-					companybalance = botmodel.attributes.company.currentbalance,
-					companyprofit = botmodel.attributes.company.profit,
-					companystart = botmodel.attributes.company.startingbalance,
-					companyid = botmodel.attributes.company.id;
-				companymodel = {id: companyid, name: companyname, startingbalance: companystart, currentbalance: companybalance, profit: companyprofit} 
-				console.log('companymodel: ', companymodel);
-				var companygrab = new Backbone.Model(companymodel);
-				var companyview = new App.Views.CompanyAssocView({model: companygrab});	
-			}
+			// if (botmodel.attributes.companyId != 0) {
+			// 	var companyname = botmodel.attributes.company.name,
+			// 		companybalance = botmodel.attributes.company.currentbalance,
+			// 		companyprofit = botmodel.attributes.company.profit,
+			// 		companystart = botmodel.attributes.company.startingbalance,
+			// 		companyid = botmodel.attributes.company.id;
+			// 	companymodel = {id: companyid, name: companyname, startingbalance: companystart, currentbalance: companybalance, profit: companyprofit} 
+			// 	console.log('companymodel: ', companymodel);
+			// 	var companygrab = new Backbone.Model(companymodel);
+			// 	var companyview = new App.Views.CompanyAssocView({model: companygrab});	
+			// }
 
-			if (botmodel.attributes.userId != 0) {	
-				var username = botmodel.attributes.user.username,
-					usercompany = botmodel.attributes.user.companyname,
-					userbalance = botmodel.attributes.user.currentbalance,
-					userprofit = botmodel.attributes.user.profit,
-					userid = botmodel.attributes.user.id;
-				usermodel = {id: userid, username: username, companyname: usercompany, currentbalance: userbalance, profit: userprofit}
-				console.log('usermodel: ', usermodel);
-				var usergrab = new Backbone.Model(usermodel);
-				var userview = new App.Views.UserAssocView({model: usergrab});
-			}
+			// if (botmodel.attributes.userId != 0) {	
+			// 	var username = botmodel.attributes.user.username,
+			// 		usercompany = botmodel.attributes.user.companyname,
+			// 		userbalance = botmodel.attributes.user.currentbalance,
+			// 		userprofit = botmodel.attributes.user.profit,
+			// 		userid = botmodel.attributes.user.id;
+			// 	usermodel = {id: userid, username: username, companyname: usercompany, currentbalance: userbalance, profit: userprofit}
+			// 	console.log('usermodel: ', usermodel);
+			// 	var usergrab = new Backbone.Model(usermodel);
+			// 	var userview = new App.Views.UserAssocView({model: usergrab});
+			// }
 
 			if (botmodel.attributes.stock) {
 				var stockname = botmodel.attributes.stock.name,
